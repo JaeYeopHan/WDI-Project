ada256d20a6d9c474f9c87a76ea60fb333cca7f0
JaeYeopHan <ljyhanll@gmail.com>
2017-05-13 19:15:39 +0900

    Add redux, react-redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './modules';
+
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
