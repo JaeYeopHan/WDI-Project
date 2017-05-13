0f67bdc3e7e7770327be3ca9fa360042a1c45749
JaeYeopHan <ljyhanll@gmail.com>
2017-05-13 16:45:53 +0900

    Change component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class componentName extends Component {
-    constructor(props) {
-        super(props);
-    }
+const Header = () => {
+    return (
+        <h1>World Development Indicator</h1>
+    );
+};
 
-    render() {
-        return (
-            <h1>React with Express hot module replacement!</h1>
-        );
-    }
-}
-
-export default componentName;
+export default Header;
