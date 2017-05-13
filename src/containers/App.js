9a5293825b9e849044da6344d63b5b716831486f
JaeYeopHan <ljyhanll@gmail.com>
2017-05-13 19:11:18 +0900

    first commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import Header from './components/Header';
-
-import './App.css'
+import Header from '../components/Header';
 
 class App extends Component {
     render() {
