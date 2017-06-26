8907c45af2616776c10b78ac222aa9148cc21d90
JaeYeopHan <ljyhanll@gmail.com>
2017-06-26 21:02:29 +0900

    Add app.test.js

--- test/component/app.test.js
+++ test/component/app.test.js
@@ -1,3 +1,14 @@
-/**
- * Created by Jbee on 2017. 6. 26..
- */
+import { renderComponent , expect } from '../test_helper';
+import App from '../../src/containers/App';
+
+describe('App.js Test' , () => {
+    let component;
+
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
+
+    it('App render test', () => {
+        expect(component).to.exist;
+    });
+});
