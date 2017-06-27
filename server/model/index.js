068876c02448a3d118e8b487778e5f0795f2f081
JaeYeopHan <ljyhanll@gmail.com>
2017-06-27 15:23:37 +0900

    Add api to board following option

--- server/model/index.js
+++ server/model/index.js
@@ -3,3 +3,8 @@ import mongoose, { Schema } from 'mongoose';
 export const Test = mongoose.model('test', new Schema({
     name: String
 }));
+
+export const FetchDataOfOption = mongoose.model('', new Schema({
+    category: String,
+    country: String
+}));
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { Test } from '../model';
+import { Test, FetchDataOfOption } from '../model';
 
 const router = express.Router();
 
+//Test api
 router.get('/get', (req, res) => {
     Test.find((err, values) => {
         if (err) {
@@ -12,6 +13,7 @@ router.get('/get', (req, res) => {
     });
 });
 
+//Test api
 router.get('/post', (req, res) => {
     const test = new Test();
     test.name = 'Jbee';//test data
@@ -23,6 +25,20 @@ router.get('/post', (req, res) => {
         }
         res.send({ result: 1 });
     });
+});
+
+router.get('/api/board/:option', (req, res) => {
+    const { category, country } = req.param.option;
+    console.log(`category: ${category}`);
+    console.log(`country: ${country}`);
+    FetchDataOfOption.find({ category, country }, { xField: 0, yField: 0 }, (err, values) => {
+        if (err) {
+            console.error(`Cannot find to category: ${category} and country: ${country}`);
+            return res.status(500).send({ error: 'Failed find' });
+        }
+        res.json(values);
+    })
+
 });
 
 export default router;
